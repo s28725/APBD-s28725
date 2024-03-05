4465dfcabc84bb407591b73be0a420e19ccf3d33
s28725 <pylyp.sviatoslav@gmail.com>
2024-03-05 14:50:25 +0100

    Add project files.

---
+++ ConsoleApp1/Program.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp1
+{
+    internal class Program
+    {
+        public static void Main()
+        {
+            int int1;
+            Console.Write("\n\n");
+            Console.Write("Check whether first number bigger than 0:\n");
+            Console.Write("-------------------------------------------");
+            Console.Write("\n\n");
+            Console.Write("Input 1st number: ");
+            int1 = Convert.ToInt32(Console.ReadLine());
+
+
+            if (int1 > 0)
+                Console.WriteLine("The number " + int1 + " is bigger than 0");
+            else
+                Console.WriteLine("The number " + int1 + " is less than 0");
+
+        }
+    }
+}
