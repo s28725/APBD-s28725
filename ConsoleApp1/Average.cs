95c79b9f5f9da95ba8a717b905d9e3f224ec3f14
s28725 <s28725@pjwstk.edu.pl>
2024-03-12 15:00:27 +0100

    added searching for biggest number in array

---
+++ ConsoleApp1/Average.cs
@@ -0,0 +1,47 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp1
+{
+    internal class MathUtils
+    {
+        public static double CalculateAverage(int[] numbers)
+        {
+            if (numbers == null || numbers.Length == 0)
+            {
+                throw new ArgumentException("The array cannot be null or empty.");
+            }
+
+            int sum = 0;
+            foreach (int num in numbers)
+            {
+                sum += num;
+            }
+
+            return (double)sum / numbers.Length;
+        }
+
+
+        public static int FindMaxValue(int[] numbers)
+        {
+            if (numbers == null || numbers.Length == 0)
+            {
+                throw new ArgumentException("The array cannot be null or empty.");
+            }
+
+            int maxValue = numbers[0];
+            foreach (int num in numbers)
+            {
+                if (num > maxValue)
+                {
+                    maxValue = num;
+                }
+            }
+
+            return maxValue;
+        }
+    }
+}
